a2188440a2b24042306fa46aa245e98f3916aba5
TigranVard <146510780+TigranVard@users.noreply.github.com>
2024-04-20 16:45:10 +1000

    Add files via upload

---
+++ Zadanka 2.py
@@ -0,0 +1,14 @@
+import random
+
+
+list = []
+# list = [random.randint(-10,10) for i in range(5)]
+
+for i in range(0,5):
+    list.append(random.randint(-10,10))
+
+if list[i] < 0 and list[ih] > 0:
+    print("знак поменялся")
+
+
+print(list[0])
---
+++ zadanka 1.py
@@ -0,0 +1,11 @@
+list = [3,8,2,7,9]
+
+n = min(list)
+
+print(n)
+
+list.remove(n)
+
+n = min(list)
+
+print(n)
